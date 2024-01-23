1c631a06207ff1a712c4275a290c77ec0b576233
rafael-mj <rafaeljakubovsky@gmail.com>
2024-01-23 03:02:24 -0300

    fix: solve refresh page error

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideHttpClient(
-      withFetch(),
-    ),
-    provideRouter(routes,
-      withInMemoryScrolling({
-        scrollPositionRestoration: 'enabled',
-        anchorScrolling: 'enabled',
-      }),
-      ),
-    provideClientHydration()]
+  providers: [provideRouter(routes, withHashLocation())]
 };
