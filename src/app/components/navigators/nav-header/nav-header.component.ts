5da0412433fc947644a1d768a7ad37bff23b7a77
rafael-mj <rafaeljakubovsky@gmail.com>
2024-01-27 04:21:02 -0300

    feat: add animations

--- src/app/components/navigators/nav-header/nav-header.component.ts
+++ src/app/components/navigators/nav-header/nav-header.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 
+import { ScreenSupport, Screens } from '../../../utils/ScreenUtils'
 @Component({
   selector: 'app-nav-header',
   templateUrl: './nav-header.component.html',
   styleUrls: ['./nav-header.component.css']
 })
 export class NavHeaderComponent {
+  @ViewChildren('n0, n1, n2, n3') elementos!: QueryList<ElementRef>;
+  htmlElements: ElementRef[] = [];
 
+  ngAfterViewInit() {
+    this.htmlElements = this.elementos.toArray();
+  }
+
+  ngAfterViewChecked() {
+    let currentScreen = ScreenSupport.getCurrentScreen();
+
+    switch(currentScreen) {
+      case Screens.Main:
+        this.htmlElements[0].nativeElement.classList.add('enabled');
+        break;
+      case Screens.Knowledge:
+        this.htmlElements[1].nativeElement.classList.add('enabled');
+        break;
+      case Screens.Skills:
+        this.htmlElements[2].nativeElement.classList.add('enabled');
+        break;
+      case Screens.Info:
+        this.htmlElements[3].nativeElement.classList.add('enabled');
+        break;
+    }
+  }
+
+  enableEffect(index: number) {
+    for (let i = 0; i < this.htmlElements.length; i++) {
+      const element = this.htmlElements[i].nativeElement;
+
+      if (i === index) {
+        element.classList.add('enabled');
+      } else {
+        element.classList.remove('enabled');
+      }
+    }
+  }
 }
