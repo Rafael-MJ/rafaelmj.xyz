f27be198d2a1938383fdd642bbe71f38e6c0ab15
rafael-mj <rafaeljakubovsky@gmail.com>
2024-02-22 03:47:30 -0300

    feat: add scr components to main screen

--- src/app/components/screens/scr-main/scr-main.component.ts
+++ src/app/components/screens/scr-main/scr-main.component.ts
@@ -5,11 +5,25 @@ import { ScreenSupport, Screens } from 'src/app/utils/ScreenUtils';
 
 import { GenArrowsComponent } from '../../general/gen-arrows/gen-arrows.component';
 import { GenDeviconsComponent } from '../../general/gen-devicons/gen-devicons.component';
+import { ScrKnowledgeComponent } from '../scr-knowledge/scr-knowledge.component';
+import { ScrSkillsComponent } from '../scr-skills/scr-skills.component';
+import { GenInfoComponent } from '../../general/gen-info/gen-info.component';
+import { ScrAboutComponent } from '../scr-about/scr-about.component';
+import { ScrInfoComponent } from '../scr-info/scr-info.component';
 
 @Component({
   selector: 'app-scr-main',
   standalone: true,
-  imports: [commonModuleUtils, GenArrowsComponent, GenDeviconsComponent],
+  imports: [
+    commonModuleUtils,
+    ScrAboutComponent,
+    ScrInfoComponent,
+    ScrKnowledgeComponent,
+    ScrSkillsComponent,
+    GenArrowsComponent,
+    GenDeviconsComponent,
+    GenInfoComponent,
+  ],
   templateUrl: './scr-main.component.html',
   styleUrl: './scr-main.component.scss',
 })
