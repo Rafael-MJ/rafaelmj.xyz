c53ed6766bcbc6ec392260a7f343bd4a46839563
rafael-mj <rafaeljakubovsky@gmail.com>
2024-02-22 04:20:21 -0300

    refactor: remove unused routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,5 @@
 import { Routes } from '@angular/router';
 
-import { ScrAboutComponent } from './components/screens/scr-about/scr-about.component';
-import { ScrSkillsComponent } from './components/screens/scr-skills/scr-skills.component';
-import { ScrKnowledgeComponent } from './components/screens/scr-knowledge/scr-knowledge.component';
-import { ScrInfoComponent } from './components/screens/scr-info/scr-info.component';
 import { ScrNotfoundComponent } from './components/screens/scr-notfound/scr-notfound.component';
 import { ScrCurriculumComponent } from './components/screens/scr-curriculum/scr-curriculum.component';
 import { ScrMainComponent } from './components/screens/scr-main/scr-main.component';
@@ -18,22 +14,6 @@ export const routes: Routes = [
   {
     path: 'main',
     component: ScrMainComponent,
-  },
-  {
-    path: 'about',
-    component: ScrAboutComponent,
-  },
-  {
-    path: 'skills',
-    component: ScrSkillsComponent,
-  },
-  {
-    path: 'knowledge',
-    component: ScrKnowledgeComponent,
-  },
-  {
-    path: 'infos',
-    component: ScrInfoComponent,
   },
   {
     path: 'curriculum',
